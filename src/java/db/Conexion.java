f652eebbf8b251bcc0fe4f978c899de1990db513
“cristian” <labso07@labso07-All-Series>
2022-03-09 10:07:44 -0600

    Este es el segundo commit subiendo el archivo a git

--- src/java/db/Conexion.java
+++ src/java/db/Conexion.java
@@ -1,45 +1,83 @@
 /*
 cristian Adair Ramirez Rodriguez
 Fecha de creación:8/03/2022, 10:43:34 AM
-Fecha de actualización:
+Fecha de actualización:09/03/2022 09:00 AM
 Descripción: Clase del producto
  */
 package db;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class Conexion {
 
-    Connection conexion;
-    String url;
-    String user;
-    String pwd;
+    static Connection conexion;
+    String url = "jdbc:postgresql://localhost/restaurantonline";
+    String user = "postgres";
+    String password = "cristian20";
 
-    public Conexion(Connection conexion, String url, String user, String pwd) {
+    public Conexion() {
+
+    }
+
+    public Conexion(Connection conexion, String url, String user, String password) {
         this.conexion = conexion;
         this.url = url;
         this.user = user;
-        this.pwd = pwd;
+        this.password = password;
     }
-    public Conexion() {
-    }
-   public void Conectar(){
-       try {
-           Conectar();
-       } catch (Exception e) {
-       }
-}
-   public void Desconectar(){
-       try {
-           Desconectar();
-       } catch (Exception e) {
-       }
-}
 
-    public Connection getConexion() {
-        return conexion;
+    public Connection Conectar() {
+        Connection con = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            conexion = DriverManager.getConnection(url,user,password);
+            System.out.println("Conexion con exito");
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+        return con;
     }
+
+    public void Desconectar() {
+        try {
+            conexion.close();
+            System.out.println("Conexion cerrada");
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+
+    }
+
     public static void main(String[] args) {
-        
+        Connection conec = null;
+        Conexion con = new Conexion();
+        con.Conectar();
+       
+        try{
+          
+            String sql="select * from producto";
+             ResultSet rs;
+             Statement st= conec.createStatement();
+             rs=st.executeQuery(sql);
+             while(rs.next()){
+                 System.out.println(rs.getObject("codigo"));
+                 System.out.println(rs.getObject("nombre"));
+                 System.out.println(rs.getObject("descripcion"));
+                 System.out.println(rs.getObject("precio"));
+                 System.out.println(rs.getObject("FechaCreacion"));
+                 System.out.println(rs.getObject("FechaActualizacion"));
+                 System.out.println(rs.getObject("FechaEliminacion"));
+                 
+                 
+             }
+             
+        }catch(Exception e){
+            System.out.println(e.toString());
+       }
+         con.Desconectar();
     }
+
 }
