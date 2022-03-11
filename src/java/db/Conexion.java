a89831ed4a8ae99318c16617e17fc96745b1d574
“cristian” <labso07@labso07-All-Series>
2022-03-11 10:18:00 -0600

    Restauranteonline guardado

--- src/java/db/Conexion.java
+++ src/java/db/Conexion.java
@@ -58,7 +58,8 @@ public class Conexion {
        
         try{
           
-            String sql="select * from restaurantonline";
+            String sql="select * from restaurantonline"
+                    + "";
              ResultSet rs;
              Statement st= conec.createStatement();
              rs=st.executeQuery(sql);
