b1d409f424425c35fc6b7aa4018f6bb4059c9740
“cristian” <labso07@labso07-All-Series>
2022-03-15 10:09:30 -0600

    Metodos eliminar, actualizar y actualizar+

--- src/java/src/model/UsuarioModelImpl.java
+++ src/java/src/model/UsuarioModelImpl.java
@@ -1,99 +1,136 @@
 /*
     Fecha de creación : 8/03/2022, 11:45:34 AM
     Author: Cristian Adair Ramirez Rodriguez
-    Fecha de actualizacion:8/03/2022, 11:50:34 AM
+    Fecha de actualizacion:15/03/2022, 11:50:34 AM
     Descripcion:Usuarios
  */
 package src.model;
 
+import Entity.Usuario;
+import bd.Conexion;
 import db.Conexion;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
-import javax.swing.JOptionPane;
 import src.entity.Usuario;
 
-/**
- *
- * @author labso07
- */
-public class UsuarioModelImpl implements IUsuarioModel {
+public class UsuarioModelmpl implements IUsuarioModel {
 
     private Conexion conexion;
     private Connection connection;
 
     @Override
-    public void crearRegistro(Usuario usuario) {
+    public void crearRegisto(Usuario usuario) {
         try {
             conexion = new Conexion();
             conexion.Conectar();
             connection = conexion.getConnection();
-            String sql = "INSERT INTO usuario(nombre) VALUES()";
-
-        } catch (Exception e) {
+            String sql = "insert into usuario(nombre_usuario, contraseña, nombre, sexo, edad) values(?,?,?,?,?);";
+            PreparedStatement st = connection.prepareStatement(sql);
+            st.setString(1, usuario.getnombre_usuario());
+            st.setString(2, usuario.getcontraseña());
+            st.setString(3, usuario.getNombre());
+            st.setString(4, usuario.getSexo());
+            st.setInt(5, usuario.getEdad());
+            st.executeUpdate();
+            conexion.Desconectar();
+        } catch (Exception ex) {
+            System.out.println("Error de crear registro= " + ex);
         }
-        try ( PreparedStatement statement = conecction.prepareStatement(sql)) {
-            statement.get String
-            (1,usuario.getNombre()
-            );
-         statement.executeUpdate();
-        }
-        
-        
-    
+    }
 
-    public static void main() { //Metodo que ayuda a dar de alta los productos
+    @Override
+    public void actualizarRegistro(Usuario usuario) {
         try {
-
-            String codigo = "001";
-            String nombre = "Huevos con jamon";
-            String descripcion = "Desayunos";
-            float precio = (float) 50.50;
-            String FechaCreacion = "08/03/2022";
-            String FechaActualizacion = "10/03/2022";
-            String FechaEliminacion = "20/03/2022";
-
-            String sql = "INSERT INTO producto (codigo, nombre, descripcion, precio, FechaCreacion, FechaActualizacion, FechaEliminacion) values(?, ?, ?, ?, ?, ?,?);";
-
-            PreparedStatement ps = conexion.prepareStatement(sql);
-            //Se obtinen los datos de los campos
-            ps.setString(1, codigo);
-            ps.setString(2, nombre);
-            ps.setString(3, descripcion);
-            ps.setFloat(4, precio);
-
-            ps.setString(5, FechaCreacion);
-            ps.setString(6, FechaActualizacion);
-            ps.setString(7, FechaCreacion);
-            ps.setString(5, FechaEliminacion);
-            ps.executeUpdate();//Se insertan los datos en la base de datos
-            JOptionPane.showMessageDialog(null, "datos insertados correctamente");
-            conexion.close();
-
-        } catch (Exception e) {
-            System.out.println("error");
-
+            conexion = new Conexion();
+            conexion.Conectar();
+            connection = conexion.getConnection();
+            String sql = "update usuario set nombre_usuario=?, contraseña=?, nombre=?, sexo=?, edad=? where codigo=" + usuario.getcodigo() + ";";
+            PreparedStatement st = connection.prepareStatement(sql);
+            st.setString(1, usuario.getNombre_Usuario());
+            st.setString(2, usuario.getContraseña());
+            st.setString(3, usuario.getNombre());
+            st.setString(4, usuario.getSexo());
+            st.setInt(5, usuario.getEdad());
+            st.executeUpdate();
+            conexion.Desconectar();
+        } catch (Exception ex) {
+            System.out.println("Error de actualizar registro= " + ex);
         }
-    }
-
-    @Override
-    public void actualizaRegistro(Usuario usuario) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public void eliminarRegistro(int idUsuario) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
     @Override
     public List<Usuario> obtenerRegistros() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        Usuario usu = new Usuario();
+        List<Usuario> lista = new ArrayList<Usuario>();
+        try {
+            conexion = new Conexion();
+            conexion.Conectar();
+            connection = conexion.getConnection();
+            String sql = "select * from usuario;";
+            Statement s = connection.createStatement();
+            ResultSet rs = s.executeQuery(sql);
+            while (rs.next()) {
+                usu = new Usuario(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getInt(6));
+                lista.add(usu);
+            }
+            conexion.Desconectar();
+        } catch (Exception ex) {
+            System.out.println("Error al obtener registros= " + ex);
+        }
+        return null;
     }
 
     @Override
     public Usuario obtenerRegistro(int idUsuario) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        Usuario usu1 = new Usuario();
+        List<Usuario> lista = new ArrayList<Usuario>();
+        try {
+            conexion = new Conexion();
+            conexion.Conectar();
+            connection = conexion.getConnection();
+            String sql = "select from usuario where codigo=" + idUsuario + ";";
+            Statement s = connection.createStatement();
+            ResultSet rs = s.executeQuery(sql);
+            while (rs.next()) {
+                usu1 = new Usuario(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getInt(6));
+                lista.add(usu1);
+            }
+            conexion.Desconectar();
+        } catch (Exception ex) {
+            System.out.println("Error de obetener registro= " + ex);
+        }
+        return null;
+    }
+
+    @Override
+    public void eliminarResgistro(int idUsuario) {
+        try {
+            conexion = new Conexion();
+            conexion.Conectar();
+            connection = conexion.getConnection();
+            String sql = "delete from usuario where codigo=" + idUsuario + ";";
+            PreparedStatement st = connection.prepareStatement(sql);
+            st.executeUpdate();
+            conexion.Desconectar();
+        } catch (Exception ex) {
+            System.out.println("Error de eliminar registro= " + ex);
+        }
+    }
+
+    public static void main(String[] args) {
+        Usuario a = new Usuario();
+        a.setcodigo("1");
+        a.setnombre_usuario("Roberto");
+        a.setcontraseña("Rober123");
+        a.setNombre("Roberto");
+        a.setSexo("Hombre");
+        a.setEdad(18);
+        UsuarioModelmpl um = new UsuarioModelmpl();
+        um.obtenerRegistros();
+//        um.actualizarRegistro(a);
+//        um.eliminarResgistro(1);
+        System.out.println(um.obtenerRegistro(1));
+        System.out.println(um.obtenerRegistros());
     }
 }
